e22c65ae24d70d707d3089bdb2d6ad3cc7f88520
sudoCreate258 <59755293+sudoCreate258@users.noreply.github.com>
2024-09-19 04:30:59 -0400

    Update exceptions.py

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,7 +1,5 @@
 class SizeConstraintError(Exception):
-    """Raised when the input does not meet the size constraints."""
     pass
 
 class AlphaNumericError(Exception):
-    """Raised when the input does not meet the alphanumeric constraints."""
     pass
