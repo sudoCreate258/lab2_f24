483ca4bfc641709822c0da97f30f221cea72647c
sudoCreate258 <59755293+sudoCreate258@users.noreply.github.com>
2024-09-19 13:26:23 -0400

    Update c_cipher.py

--- src/c_cipher.py
+++ src/c_cipher.py
@@ -6,10 +6,10 @@ def encrypt(email="abc012"):
     TODO: What is the objective? 
 
     Args:
-        TODO: what argument data types are expected? (i.e., email)
+        TODO: what arguments and data types are expected? (i.e., email)
 
     Returns:
-        TODO: what data types are being returned?   
+        TODO: what varibale and data types are being returned?   
     """
     output = "" 
     len_flag = len(email) != 6
@@ -21,13 +21,12 @@ def encrypt(email="abc012"):
     #     enum_flag = A or B
     anum_flag = email[:3] != 'abc' or email[3:] != '012' 
 
-    # input validation
-    if len_flag:
+    if len_flag:                         # NOTE: here we provide input validation on length
         output = "Length check failed\n"
         output += "Email must be 6 characters long."
         logging.info(output)
         return output        
-    if anum_flag:
+    if anum_flag:                        # NOTE: here we provide input validation on alpha/num
         output = "alpha num check failed\n"
         output += "Email must have 3 letters followed by 3 digits."
         logging.info(output)
@@ -36,13 +35,12 @@ def encrypt(email="abc012"):
     # TODO: fix line below, process our string into a list
     email_lst = ["a", "b", "c", "0", "1", "2"]
         
-    # TODO: fix line(s) below, update EACH element's ASCII value + 3
-    new_ascii = ord(email_lst[0]) + 3
-    email_lst[0] = chr(new_ascii)
+    # TODO: complete line(s) below, convert EACH new element into a string
+    new_ascii = ord(email_lst[0]) + 3    # NOTE: here we extract and update element at 0 
+    email_lst[0] = chr(new_ascii)        # NOTE: here we convert our ASCII into string
         
     # TODO: fix line below, convert list into a string
     email_str = "dbc012"
-
 
     # keep all updates in the retVal (str) variablei
     # i.e.,
@@ -57,12 +55,38 @@ def decrypt(email="def345"):
     TODO: What is the objective? 
 
     Args:
-        TODO: what argument data types are expected? (i.e., email)
+        TODO: what arguments and data types are expected? (i.e., email)
 
     Returns:
-        TODO: what data types are being returned?   
+        TODO: what varibale and data types are being returned?   
     """
     # input validation
-    # TODO: implement encrypt pseudocode but shift down 3
-    # 
+    output = "" 
+    len_flag = len(email) != 6
+    # TODO: fix line below and, implement functionality rather than literals
+    # keep all updates in the anum_flag (bool) variable
+    # i.e., 
+    #     A = email[:3] (check first half)
+    #     B = email[3:] (check second half)
+    #     enum_flag = A or B
+    anum_flag = email[:3] != 'abc' or email[3:] != '012' 
+
+    if len_flag:                         # NOTE: here we provide input validation on length
+        output = "Length check failed\n"
+        output += "Email must be 6 characters long."
+        logging.info(output)
+        return output        
+    if anum_flag:                        # NOTE: here we provide input validation on alpha/num
+        output = "alpha num check failed\n"
+        output += "Email must have 3 letters followed by 3 digits."
+        logging.info(output)
+        return output   
+        
+    # keep all updates in the retVal (str) variablei
+    # i.e.,
+    #    email_str = " some string updates here "
+    #    email_1 = email_str.strip()
+    #    retVal = email_1
+    retVal = "aef345"
+    return retVal
     pass
