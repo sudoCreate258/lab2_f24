bf2efdad68af100af4bdf44802ca288845ad460b
sudoCreate258 <59755293+sudoCreate258@users.noreply.github.com>
2024-09-19 12:07:51 -0400

    Update c_cipher.py
    simplified ...

--- src/c_cipher.py
+++ src/c_cipher.py
@@ -1,5 +1,4 @@
-from src.exceptions import SizeConstraintError, AlphaNumericError
-
+# TODO: implement encrypt pseudocode and shift up 3
 def encrypt(email="abc012"):
     """
     TODO: What is the objective? 
@@ -14,46 +13,35 @@ def encrypt(email="abc012"):
         SizeConstraintError: If the email is not exactly 6 characters long.
         AlphaNumericError: If the email does not consist of 3 letters followed by 3 digits.
     """
-    try:
-        len_flag = len(email) != 6
-        anum_flag = email[:3] != 'abc' or email[3:] != '012' 
-        # TODO: fix line above, what built in functions can we implement?
-        
-        print('len_flag',len_flag,email)
-        if len_flag:
-            print("Length check failed",len(email))
-            raise SizeConstraintError("Email must be 6 characters long.")
-            
-        print('anum_flag',anum_flag,email[:3],email[3:])
-        if anum_flag:
-            print("alphanum check failed")
-            print(email[:3].isalpha(), email[3:].isdecimal())
-            raise AlphaNumericError("Email must have 3 letters followed by 3 digits.")
+    
+    len_flag = len(email) != 6
+    anum_flag = email[:3] != 'abc' or email[3:] != '012' 
+    # TODO: fix line above, what built in functions can we implement rather than only using literals?
 
-        # TODO: fix line below, process our string into a list
-        email_lst = ["a", "b", "c", "0", "1", "2"]
-        
-        # TODO: fix line(s) below, update EACH element's ASCII value + 3
-        new_ascii = ord(email_lst[0]) + 3
-        email_lst[0] = chr(new_ascii)
-        
-        # TODO: fix line below, convert list into a string
-        email_str = "dbc012"
-        retVal = email_str
-
-        return retVal 
-
-    except SizeConstraintError as e: 
-        print(f"SizeConstraintError: {e}")
-        output = "Error"
-        # TODO: fix line above, use string formatting for a specific error message
+    output = "" 
+    if len_flag:
+        output = "Length check failed\n"
+        output += "Email must be 6 characters long."
         print(output)
-        return output 
-    except AlphaNumericError as e:
-        output = "Error"
-        # TODO: fix line above, use string formatting for a specific error message
+    if num_flag:
+        output = "alpha num check failed\n"
+        output += "Email must have 3 letters followed by 3 digits."
         print(output)
-        return output
+        
+    # TODO: fix line below, process our string into a list
+    email_lst = ["a", "b", "c", "0", "1", "2"]
+        
+    # TODO: fix line(s) below, update EACH element's ASCII value + 3
+    new_ascii = ord(email_lst[0]) + 3
+    email_lst[0] = chr(new_ascii)
+        
+    # TODO: fix line below, convert list into a string
+    email_str = "dbc012"
 
+    
+    retVal = email_str
+    return retVal 
+
+# TODO: implement encrypt pseudocode but shift down 3
 def decrypt(email="def345"):
     pass
