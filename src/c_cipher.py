d800749bb509b33c2b227118e879549ff7aa4be2
sudoCreate258 <59755293+sudoCreate258@users.noreply.github.com>
2024-09-19 12:45:08 -0400

    Update c_cipher.py

--- src/c_cipher.py
+++ src/c_cipher.py
@@ -1,7 +1,6 @@
 import logging
 logging.basicConfig(level=logging.INFO, format='%(message)s')
 
-# TODO: implement encrypt pseudocode and shift up 3
 def encrypt(email="abc012"):
     """
     TODO: What is the objective? 
@@ -11,17 +10,18 @@ def encrypt(email="abc012"):
 
     Returns:
         TODO: what data types are being returned?   
-
-    Raises:
-        SizeConstraintError: If the email is not exactly 6 characters long.
-        AlphaNumericError: If the email does not consist of 3 letters followed by 3 digits.
     """
     output = "" 
     len_flag = len(email) != 6
-    # TODO: fix line below, implement functionality rather than literals
+    # TODO: fix line below and, implement functionality rather than literals
+    # keep all updates in the anum_flag (bool) variable
+    # i.e., 
+    #     A = email[:3] (check first half)
+    #     B = email[3:] (check second half)
+    #     enum_flag = A or B
     anum_flag = email[:3] != 'abc' or email[3:] != '012' 
 
-    # simple input validation
+    # input validation
     if len_flag:
         output = "Length check failed\n"
         output += "Email must be 6 characters long."
@@ -31,7 +31,8 @@ def encrypt(email="abc012"):
         output = "alpha num check failed\n"
         output += "Email must have 3 letters followed by 3 digits."
         logging.info(output)
-        return output          
+        return output     
+        
     # TODO: fix line below, process our string into a list
     email_lst = ["a", "b", "c", "0", "1", "2"]
         
@@ -42,10 +43,26 @@ def encrypt(email="abc012"):
     # TODO: fix line below, convert list into a string
     email_str = "dbc012"
 
-    
+
+    # keep all updates in the retVal (str) variablei
+    # i.e.,
+    #    email_str = " some string updates here "
+    #    email_1 = email_str.strip()
+    #    retVal = email_1
     retVal = email_str
     return retVal 
 
-# TODO: implement encrypt pseudocode but shift down 3
 def decrypt(email="def345"):
+    """
+    TODO: What is the objective? 
+
+    Args:
+        TODO: what argument data types are expected? (i.e., email)
+
+    Returns:
+        TODO: what data types are being returned?   
+    """
+    # input validation
+    # TODO: implement encrypt pseudocode but shift down 3
+    # 
     pass
