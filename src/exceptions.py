9a3357bccab09ae7a0de45ee61fbd9f19912d2dc
sudoCreate258 <59755293+sudoCreate258@users.noreply.github.com>
2024-09-19 01:48:18 -0400

    Create exceptions.py
    updated our custom exceptions

---
+++ src/exceptions.py
@@ -0,0 +1,7 @@
+class SizeConstraintError(Exception):
+    """Raised when the input does not meet the size constraints."""
+    pass
+
+class AlphaNumericError(Exception):
+    """Raised when the input does not meet the alphanumeric constraints."""
+    pass
